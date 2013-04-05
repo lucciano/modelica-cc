862147b1265cae2ade52525bdf19898d9cca9695
fbergero2 <fbergero2@4bb17074-bfce-49f5-bcda-e32d08ab1a5a>
2013-04-05 18:16:15 +0000

    Moving symbol table
    git-svn-id: svn://svn.code.sf.net/p/modelicacc/code/trunk@12 4bb17074-bfce-49f5-bcda-e32d08ab1a5a

--- util/symbol_table.h
+++ util/symbol_table.h
@@ -18,31 +18,13 @@
 
 ******************************************************************************/
 
-#include <flatten/symbol_table.h>
-#include <stdlib.h>
+#ifndef SYMBOL_TABLE
+#define SYMBOL_TABLE
 
-void SymbolTable::addSymbol(string name, AST_Class cl) {
-  _st[name]=cl;
+#include <map>
+#include <string>
+#include <ast/ast_builder.h>
 
-}
+typedef std::map<std::string, AST_Component> VariableName;
 
-AST_Class SymbolTable::lookup(string name) {
-
-  map<string, AST_Class>::iterator it;
-  it = _st.find(name);
-  if (it==_st.end()) {
-   cerr << "Failed lookup of type: " << name << endl;
-   exit(-1);
-  }
-  return it->second;
-}
-
-void SymbolTable::dump() {
-  map<string, AST_Class>::iterator it;
-  cerr << "Valid types = {";
-  for(it=_st.begin();it!=_st.end();it++) {
-    cerr << it->first << " ";
-  }
-  cerr << "}" << endl;
-
-}
+#endif
