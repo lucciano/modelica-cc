52279ccfcfb2e597b335a978aa35c1abb43e16f7
fbergero2 <fbergero2@4bb17074-bfce-49f5-bcda-e32d08ab1a5a>
2013-05-13 23:27:32 +0000

    Adding antialias functions
    git-svn-id: svn://svn.code.sf.net/p/modelicacc/code/trunk@70 4bb17074-bfce-49f5-bcda-e32d08ab1a5a

---
+++ antialias_/find_state.cpp
@@ -0,0 +1,98 @@
+/*
+ * findstate.cpp
+ *
+ *  Created on: 28/04/2013
+ *      Author: fede
+ */
+
+#include <iostream>
+
+#include <causalize/find_state.h>
+#include <ast/ast_builder.h>
+#include <ast/expression.h>
+#include <util/symbol_table.h>
+
+StateVariablesFinder::StateVariablesFinder(MMO_Class *c) {
+	_c = c;
+	_varSymbolTable = c->getVarSymbolTable();
+}
+
+StateVariablesFinder::~StateVariablesFinder() {
+	// TODO [Fede] Auto-generated destructor stub
+}
+
+#define IS_CREF(X) ((X)->expressionType()==EXPCOMPREF)
+#define IS_UMINUS(X) ((X)->expressionType()==EXPUMINUS)
+#define IS_UMINUS_VAR(X) (IS_UMINUS(X) && IS_CREF((X)->getAsUMinus()->exp()))
+#define IS_VAR(X) (IS_CREF(X) || IS_UMINUS_VAR(X))
+#define IS_ZERO_REAL(X) ((X)->expressionType()==EXPREAL && (X)->getAsReal()->val()==0.0)
+#define IS_ZERO_INT(X) ((X)->expressionType()==EXPINTEGER && (X)->getAsInteger()->val()==0)
+#define IS_ZERO(X) (IS_ZERO_REAL(X) || IS_ZERO_INT(X))
+#define IS_SUM(X) ((X)->expressionType()==EXPBINOP && ((X)->getAsBinOp()->binopType()==BINOPADD || (X)->getAsBinOp()->binopType()==BINOPSUB ) \
+                    && (IS_VAR((X)->getAsBinOp()->left()) && IS_VAR((X)->getAsBinOp()->right())))
+
+void StateVariablesFinder::findStateVariables() {
+
+	MMO_EquationList eqs = _c->getEquations();
+	MMO_EquationListIterator eqit;
+
+		if (eqs != NULL) {
+			foreach(eqit, eqs) {
+				AST_Equation eq = (AST_Equation) current(eqit);
+				switch(eq->equationType()) {
+					case EQEQUALITY:
+						AST_Equation_Equality eqeq =  eq->getAsEquality();
+						mapTraverse(eqeq->left());
+						mapTraverse(eqeq->right());
+                                                IsConstant ic(_varSymbolTable);
+                                                if (IS_VAR(eqeq->left()) && IS_VAR(eqeq->right())) {
+                                                  // a = b;
+                                                  cerr << "ALIAS: "<< eqeq;
+                                                }
+                                                if (IS_ZERO(eqeq->left()) && IS_SUM(eqeq->right())) {
+                                                  // 0 =a + b;
+                                                  cerr << "SOLV_ALIAS: "<< eqeq;
+                                                }
+                                                if (IS_ZERO(eqeq->right()) && IS_SUM(eqeq->left())) {
+                                                  // a + b = 0;
+                                                  cerr << "SOLV_ALIAS: "<< eqeq;
+                                                }
+                                                if (IS_VAR(eqeq->left()) && ic.foldTraverse(eqeq->right())) {
+                                                  // a = const;
+                                                  cerr << "CONST_ALIAS: "<< eqeq;
+                                                }
+                                                if (IS_VAR(eqeq->right()) && ic.foldTraverse(eqeq->left())) {
+                                                  // const =a;
+                                                  cerr << "CONST_ALIAS: "<< eqeq;
+                                                }
+                                                  
+						break;
+					// TODO [Fede] ver como es el manejo de errores y/o excepciones.
+				}
+			}
+		}
+}
+
+AST_Expression StateVariablesFinder::mapTraverseElement(AST_Expression e) {
+	switch(e->expressionType()) {
+		case EXPDERIVATIVE:
+			AST_Expression_Derivative der = e->getAsDerivative();
+			AST_ExpressionList arguments = der->arguments();
+			AST_Expression argument = arguments->front(); // FIXME [Fede] Por ahora nos quedamos con el 1er elemento. En un futuro habra que iterar sobre todos los elementos de la lista.
+			switch(argument->expressionType()) {
+				case EXPCOMPREF:
+					AST_Expression_ComponentReference compref = argument->getAsComponentRef();
+					VarInfo *varInfo = _varSymbolTable->lookup(compref->name());
+					if (varInfo != NULL) {
+						varInfo->setState();
+					} else {
+						// TODO [Fede]
+					}
+					break;
+				// TODO [Fede] ver como es el manejo de errores y/o excepciones.
+			}
+
+	}
+	return e;
+}
+
